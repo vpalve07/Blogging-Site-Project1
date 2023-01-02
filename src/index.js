5ab2e9faba7d2f8abbfe47d14caa2f3409118662
vpalve07 <vinayak.palve02@gmail.com>
2023-01-02 12:31:23 +0530

    index-commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const route = require('../src/routes/route')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect("mongodb+srv://blogGroup17:blogGroup17@cluster0.lbcnqhr.mongodb.net/group17Database",{
+    useNewUrlParser: true
+})
+.then(() =>console.log("MongoDB is connected"))
+.catch(err=>console.log(err))
+
+app.use("/",route)
+
+app.listen(3000,function(){
+    console.log("Express app running on port 3000")
+})
